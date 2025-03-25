616bb8e5c4ac80fc93747bbf53b7b61c52d279e4
Yerdaulet <mrtlhnv@gmail.com>
2025-03-25 14:53:45 +0500

    updated post-details page

--- frontend/src/app/post-details/post-details.component.ts
+++ frontend/src/app/post-details/post-details.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { Post } from '../models';
-import { POSTS } from '../fake-db';
+import { Post, Comment } from '../models';
+import { POSTS, COMMENTS } from '../fake-db';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.css'
 })
 
 export class PostDetailsComponent {
-  post: Post = POSTS[0];
+  post: Post = POSTS[1];
+  comments: Comment[] = COMMENTS;
 }
