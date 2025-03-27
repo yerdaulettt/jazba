5717dcb94a679f6365c06a1ec991be1cefb158b5
Bekarys <abievb91@gmail.com>
2025-03-27 16:24:04 +0500

    Changes

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'] 
 })
-
-export class HomeComponent {
-}
+export class HomeComponent { }
